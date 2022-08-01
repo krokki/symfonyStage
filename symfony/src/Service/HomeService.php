070c3200396649d72e99bcbcb786f6c07a2c1166
Ivan Korost <iiilasthero@gmail.com>
2022-08-01 11:17:58 +0300

    Create db, install doctrine,

---
+++ symfony/src/Service/HomeService.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Service;
+
+class HomeService
+{
+    public function PrintText() : void {
+        echo "Print smt text";
+    }
+
+    public function AnotherText() : void {
+        echo "Print another text!!!";
+    }
+
+}
