dd6680252e940509cd77f67a331768d514a5f433
Ivan Korost <iiilasthero@gmail.com>
2022-08-04 15:09:03 +0300

    Use normalizer for object to array convert. And install follow packages.

--- symfony/src/Controller/V1/HomeController.php
+++ symfony/src/Controller/V1/HomeController.php
@@ -7,15 +7,30 @@ use App\Entity\Phone;
 use App\Service\Phone\PhoneService;
 use Doctrine\ORM\EntityManagerInterface;
 use ErrorException;
+use Normalizer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\Serializer\Exception\ExceptionInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
 
 class HomeController extends AbstractController
 {
+    private NormalizerInterface $normalizerInterface;
+
+    /**
+     * @param NormalizerInterface $ni
+     */
+    public function __construct(NormalizerInterface $ni)
+    {
+        $this->normalizerInterface = $ni;
+    }
 
     public function index(): Response
     {
@@ -44,19 +59,45 @@ class HomeController extends AbstractController
     }
 
 
-    public function addPhone()
+    public function practiceAlg(): ?Response
     {
+            $string = "man in Black   ";
+            $prepare = "";
+            $result = "";
+            $offset = strlen($string) - 1;
 
+            for($i = $offset; $i--;)
+            {
+                if($string[$i] !== ' ') break;
+            }
+
+            for($i = $offset; $i--;)
+            {
+                if($string[$i] === ' ') break;
+                $prepare .= $string[$i];
+            }
+            for($j = strlen($prepare); $j--;)
+            {
+             $result.= $prepare[$j];
+            }
+            echo $result;
+
+            return new Response("");
     }
 
 
-
-    public function  addOnePhone(Manufacturer $manufacturer, Request $request, EntityManagerInterface $em, PhoneService $phoneService): void
+    public function  addOnePhone(Manufacturer $manufacturer, Request $request, EntityManagerInterface $em, PhoneService $phoneService): JsonResponse
     {
+        //        Так тоже можно
+        //        $normalizers = [new ObjectNormalizer()];
+        //        $serializer = new Serializer($normalizers, []);
+        //        $arrPhone = $serializer->normalize($phone);
+
         $phoneSpecs = $request->toArray();
         $phone = $phoneService->addPhone($manufacturer, $phoneSpecs);
-        dd($phone);
-
+        /** @noinspection PhpUnhandledExceptionInspection */
+        $arrPhone = $this->normalizerInterface->normalize($phone); //object to array
+        return $this->json($arrPhone);
     }
 
 
@@ -89,4 +130,5 @@ class HomeController extends AbstractController
         //  path: /V1/manufacturers/{id}/phones
         //  controller: App\Controller\V1\HomeController::addOnePhone
 //    }
+
 }
